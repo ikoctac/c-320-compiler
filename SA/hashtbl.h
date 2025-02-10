ee6640de48050a77d5044df8ebe5100601f3e8f2
ikoctac <ikoctac@gmail.com>
2025-02-10 15:58:56 +0200

    added comments

--- SA/hashtbl.h
+++ SA/hashtbl.h
@@ -6,23 +6,23 @@
 typedef size_t hash_size;
 
 struct hashnode_s {
-	char *key;
-	void *data;
-	int scope;
-	struct hashnode_s *next;
+	char *key;	//hash key
+	void *data;	//associated data with key
+	int scope;	//scope of the hashnode 
+	struct hashnode_s *next;	//pointer to next hashnode
 };
 
 typedef struct hashtbl {
-	hash_size size;
-	struct hashnode_s **nodes;
-	hash_size (*hashfunc)(const char *);
+	hash_size size;		//hashtbl size
+	struct hashnode_s **nodes;	//array of hashnode pointers
+	hash_size (*hashfunc)(const char *);	//function pointer to hash function
 } HASHTBL;
 
 
-HASHTBL *hashtbl_create(hash_size size, hash_size (*hashfunc)(const char *));
-void hashtbl_destroy(HASHTBL *hashtbl);
-int hashtbl_insert(HASHTBL *hashtbl, const char *key, void *data, int scope);
-int hashtbl_remove(HASHTBL *hashtbl, const char *key,int scope);
-void *hashtbl_get(HASHTBL *hashtbl, int scope);
+HASHTBL *hashtbl_create(hash_size size, hash_size (*hashfunc)(const char *)); //create new hashtable
+void hashtbl_destroy(HASHTBL *hashtbl);	//destroy hashtable
+int hashtbl_insert(HASHTBL *hashtbl, const char *key, void *data, int scope); //insert data into hashtable
+int hashtbl_remove(HASHTBL *hashtbl, const char *key,int scope); //remove data from hashtable
+void *hashtbl_get(HASHTBL *hashtbl, int scope); //get data from hashtable
 
 #endif
